ff922cbaec955d67d58763f42ac155b4055e2832
Shinyanogit <ymgtsny7@gmail.com>
2024-03-07 10:08:01 +0900

    150ターン感染ストップ

---
+++ src/app/game/_functions/_policys/mask.ts
@@ -0,0 +1,15 @@
+import { ParamsModel } from "../../_params/params";
+import { GameState } from "../../_states/state";
+
+export const mask = (state: GameState, params: ParamsModel) => {
+  // const balls = params.balls;
+  const balls = [...state.balls];
+
+  for (let i = 0; i < balls.length; i++) {
+    if (balls[i].contacted) {
+      balls[i].masked = true;
+      balls[i].turnRemoveMask = state.sceneState.turns + params.MASK_DURATION;
+    }
+  }
+  return balls;
+};
