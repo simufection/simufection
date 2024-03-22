970a44ab8a2b3f0b82f4f39ec9157596f7f5a5d2
Tao119 <tao.dama.art@gmail.com>
2024-03-22 20:33:43 +0900

    fix

--- src/app/game/_functions/_policys/pcr.ts
+++ src/app/game/_functions/_policys/pcr.ts
@@ -6,29 +6,23 @@ export const pcr = (state: GameState, params: ParamsModel) => {
 
   for (let i = 0; i < balls.length; i++) {
     if (balls[i].dead) continue;
-    // 前回の検査で偽陽性だった場合は解除
-    // if (balls[i].stop && !balls[i].contacted) {
-    const condition_i = balls[i].contacted && !balls[i].healed
-    if (!condition_i&&balls[i].stop) {
-      balls[i].stop = false;
-    }
-    if (!balls[i].stop&&Math.random() < params.CHECK_INFECTED/100) {
-      // 検査実施
-    //   if ((balls[i].contacted || balls[i].reinfect) && !balls[i].healed)
-      if ((condition_i || balls[i].reinfect) && !balls[i].healed)
-       {
-        if (Math.random() < params.POSITIVE_RATE/100)
-         {
-          // 陽性
-          balls[i].stop=true;
-      }}
-      else if (Math.random() < params.FALSE_POSITIVE_RATE/100) {
-          // 偽陽性
-          balls[i].stop=true;
-          balls[i].turnReMove = state.sceneState.turns + params.TURNS_REQUIRED_FOR_RE_MOVE;
-        }
+    const condition_i = balls[i].contacted && !balls[i].healed;
 
+    if (Math.random() < params.CHECK_INFECTED) {
+      if (!condition_i && balls[i].stop) {
+        balls[i].stop = false;
+        continue;
+      }
+      if ((condition_i || balls[i].reinfect) && !balls[i].healed) {
+        if (Math.random() < params.POSITIVE_RATE) {
+          balls[i].stop = true;
+        }
+      } else if (Math.random() < params.FALSE_POSITIVE_RATE) {
+        balls[i].stop = true;
+        balls[i].turnReMove =
+          state.sceneState.turns + params.TURNS_REQUIRED_FOR_RE_MOVE;
+      }
+    }
   }
-}
-return balls;
+  return balls;
 };
