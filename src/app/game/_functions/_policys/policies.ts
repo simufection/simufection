9483e3ee338915703c4c4ad18e85c8aed179a675
Tao119 <tao.dama.art@gmail.com>
2024-04-04 16:35:05 +0900

    update policydata

--- src/app/game/_functions/_policys/policies.ts
+++ src/app/game/_functions/_policys/policies.ts
@@ -25,7 +25,7 @@ export type Policy = {
     mousePos: Position,
     sw: number
   ) => Object;
-  point: string;
+  initPoint: number;
   isActive: boolean;
   image?: StaticImageData;
   cooltime?: number;
@@ -58,128 +58,130 @@ const mapPos = (
   return { x: x, y: y };
 };
 
-export const policies: Policy[] = [
-  {
-    key: "v",
-    label: "vaccinate",
-    func: (state, params, cvsPos, mousePos, sw) => {
-      const { player } = state;
-      const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
-      if (!droppedPos) return {};
-      player.points -= params.POINTS_FOR_VACCINE;
-      const virus = vaccine(state, params);
-      const events = [...state.events];
-      events.push([
-        state.sceneState.turns,
-        "policy_v",
-        { prob: virus.prob.toFixed(2) },
-      ]);
-      return { player: player, virus: virus, events: events };
-    },
-    point: "POINTS_FOR_VACCINE",
-    isActive: true,
-    image: vaccineImage,
-    cooltime: 100,
-  },
-  {
-    key: "e",
-    label: "medicine",
-    func: (state, params, cvsPos, mousePos, sw) => {
-      const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
-      if (!droppedPos) return {};
-      const { player } = state;
-      player.points -= params.POINTS_FOR_MEDICINE;
-      const virus = medicine(state, params);
-      const events = [...state.events];
-      events.push([
-        state.sceneState.turns,
-        "policy_e",
-        {
-          healProb: virus.healProb.toFixed(2),
-        },
-      ]);
-      return { player: player, virus: virus, events: events };
-    },
-    point: "POINTS_FOR_MEDICINE",
-    isActive: true,
-    image: medicineImage,
-  },
-  {
-    key: "m",
-    label: "mask",
-    func: (state, params, cvsPos, mousePos, sw) => {
-      const { player } = state;
-      const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
-      if (!droppedPos) return {};
-      player.points -= params.POINTS_FOR_MASK;
-      const { balls, data } = mask(state, params);
-      const events = [...state.events];
-      events.push([state.sceneState.turns, "policy_m", { ...data }]);
-      return { balls: balls, events: events };
-    },
-    point: "POINTS_FOR_MASK",
-    isActive: true,
-    image: maskImage,
-  },
-  {
-    key: "l",
-    label: "lockdown",
-    func: (state, params, cvsPos, mousePos, sw) => {
-      const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
-      const { player, prefs, map, balls } = state;
-      if (!droppedPos) return {};
-      const prefId = map.map[droppedPos.x][droppedPos.y];
-      if (prefId <= 0 || prefs[prefId].isLockedDown) {
-        return {};
-      }
-      if (infectionRate(balls, prefId) < params.INFECTION_RATE_FOR_LOCKDOWN) {
+export const policies = (params: ParamsModel): Policy[] => {
+  return [
+    {
+      key: "v",
+      label: "vaccinate",
+      func: (state, params, cvsPos, mousePos, sw) => {
+        const { player } = state;
+        const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
+        if (!droppedPos) return {};
+        player.points -= params.POINTS_FOR_VACCINE;
+        const virus = vaccine(state, params);
         const events = [...state.events];
         events.push([
           state.sceneState.turns,
-          "lockdown_failure",
+          "policy_v",
+          { prob: virus.prob.toFixed(2) },
+        ]);
+        return { player: player, virus: virus, events: events };
+      },
+      initPoint: params["POINTS_FOR_VACCINE"],
+      isActive: true,
+      image: vaccineImage,
+      cooltime: 100,
+    },
+    {
+      key: "e",
+      label: "medicine",
+      func: (state, params, cvsPos, mousePos, sw) => {
+        const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
+        if (!droppedPos) return {};
+        const { player } = state;
+        player.points -= params.POINTS_FOR_MEDICINE;
+        const virus = medicine(state, params);
+        const events = [...state.events];
+        events.push([
+          state.sceneState.turns,
+          "policy_e",
           {
-            name: allPrefs.filter((row) => row.id == prefId)[0].name,
+            healProb: virus.healProb.toFixed(2),
           },
         ]);
-        return { events: events };
-      }
-      player.points -= params.POINTS_FOR_LOCKDOWN;
-      const { newPrefs } = lockdown(
-        state,
-        params,
-        prefId,
-        state.sceneState.turns
-      );
-      const events = [...state.events];
-      events.push([
-        state.sceneState.turns,
-        "policy_l",
-        {
-          name: allPrefs.filter((row) => row.id == prefId)[0].name,
-          compliance: (1 - prefs[prefId].lockdownCompliance).toFixed(2),
-        },
-      ]);
-      return { player: player, prefs: newPrefs, events: events };
+        return { player: player, virus: virus, events: events };
+      },
+      initPoint: params["POINTS_FOR_MEDICINE"],
+      isActive: true,
+      image: medicineImage,
     },
-    point: "POINTS_FOR_LOCKDOWN",
-    isActive: true,
-    image: lockDownImage,
-  },
-  {
-    key: "p",
-    label: "pcr",
-    func: (state, params, cvsPos, mousePos, sw) => {
-      const { player } = state;
-      const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
-      if (!droppedPos) return {};
-      player.points -= params.POINTS_FOR_PCR;
-      const { balls, data } = pcr(state, params);
-      const events = [...state.events];
-      events.push([state.sceneState.turns, "policy_p", { ...data }]);
-      return { player: player, balls: balls, events: events };
+    {
+      key: "m",
+      label: "mask",
+      func: (state, params, cvsPos, mousePos, sw) => {
+        const { player } = state;
+        const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
+        if (!droppedPos) return {};
+        player.points -= params.POINTS_FOR_MASK;
+        const { balls, data } = mask(state, params);
+        const events = [...state.events];
+        events.push([state.sceneState.turns, "policy_m", { ...data }]);
+        return { balls: balls, events: events };
+      },
+      initPoint: params["POINTS_FOR_MASK"],
+      isActive: true,
+      image: maskImage,
     },
-    point: "POINTS_FOR_PCR",
-    isActive: true,
-    image: pcrImage,
-  },
-];
+    {
+      key: "l",
+      label: "lockdown",
+      func: (state, params, cvsPos, mousePos, sw) => {
+        const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
+        const { player, prefs, map, balls } = state;
+        if (!droppedPos) return {};
+        const prefId = map.map[droppedPos.x][droppedPos.y];
+        if (prefId <= 0 || prefs[prefId].isLockedDown) {
+          return {};
+        }
+        if (infectionRate(balls, prefId) < params.INFECTION_RATE_FOR_LOCKDOWN) {
+          const events = [...state.events];
+          events.push([
+            state.sceneState.turns,
+            "lockdown_failure",
+            {
+              name: allPrefs.filter((row) => row.id == prefId)[0].name,
+            },
+          ]);
+          return { events: events };
+        }
+        player.points -= params.POINTS_FOR_LOCKDOWN;
+        const { newPrefs } = lockdown(
+          state,
+          params,
+          prefId,
+          state.sceneState.turns
+        );
+        const events = [...state.events];
+        events.push([
+          state.sceneState.turns,
+          "policy_l",
+          {
+            name: allPrefs.filter((row) => row.id == prefId)[0].name,
+            compliance: (1 - prefs[prefId].lockdownCompliance).toFixed(2),
+          },
+        ]);
+        return { player: player, prefs: newPrefs, events: events };
+      },
+      initPoint: params["POINTS_FOR_LOCKDOWN"],
+      isActive: true,
+      image: lockDownImage,
+    },
+    {
+      key: "p",
+      label: "pcr",
+      func: (state, params, cvsPos, mousePos, sw) => {
+        const { player } = state;
+        const droppedPos = mapPos(cvsPos, mousePos, state.map, params, sw);
+        if (!droppedPos) return {};
+        player.points -= params.POINTS_FOR_PCR;
+        const { balls, data } = pcr(state, params);
+        const events = [...state.events];
+        events.push([state.sceneState.turns, "policy_p", { ...data }]);
+        return { player: player, balls: balls, events: events };
+      },
+      initPoint: params["POINTS_FOR_PCR"],
+      isActive: true,
+      image: pcrImage,
+    },
+  ];
+};
