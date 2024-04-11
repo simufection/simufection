01a0806d78a71d2b5b356b68e9d8a9c4d7e03b96
Tao119 <tao.dama.art@gmail.com>
2024-04-11 16:10:32 +0900

    fix

--- src/app/game/_states/player.ts
+++ src/app/game/_states/player.ts
@@ -20,7 +20,7 @@ export const updatePoint = (
       (a +
         (params.MAX_DELTA_POINT - params.INITIAL_DELTA_POINT) *
           (damage_count / params.MAX_BALLS));
-  player.points += player.pt;
+  player.points = Math.min(player.points + player.pt, 10);
 
   return player;
 };
