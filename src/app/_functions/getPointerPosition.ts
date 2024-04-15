946ae08f4a1a44d784dfcf95dad7f56b58f3b125
yt0914 <yamaguchi-yuta972@g.ecc.u-tokyo.ac.jp>
2024-04-15 10:43:49 +0900

    変数名の修正

--- src/app/_functions/getPointerPosition.ts
+++ src/app/_functions/getPointerPosition.ts
@@ -1,11 +1,11 @@
 export const getPointerPosition = () => {
   return new Promise((resolve) => {
-    const mouseUpHandler = (event: PointerEvent) => {
-      window.removeEventListener("pointerup", mouseUpHandler);
+    const pointerUpHandler = (event: PointerEvent) => {
+      window.removeEventListener("pointerup", pointerUpHandler);
 
       resolve({ x: event.clientX, y: event.clientY });
     };
 
-    window.addEventListener("pointerup", mouseUpHandler);
+    window.addEventListener("pointerup", pointerUpHandler);
   });
 };
