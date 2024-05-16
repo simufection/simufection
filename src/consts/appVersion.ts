73be4db7036a9103a9652081e4ae77fa7bb2d4e7
Tao119 <tao.dama.art@gmail.com>
2024-05-16 21:52:53 +0900

    1.0.0

--- src/consts/appVersion.ts
+++ src/consts/appVersion.ts
@@ -1,13 +1,5 @@
 export const appVersion = process.env.APP_VERSION ?? "0.0.0";
 
 export const appVersions = [
-  "0.0.0",
-  "0.1.0",
-  "0.1.1",
-  "0.1.2",
-  "0.1.3",
-  "0.2.0",
-  "0.2.1",
-  "0.2.2",
-  "0.3.0",
+  "1.0.0"
 ];
