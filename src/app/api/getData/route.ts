122faf20cc22a4266b67055719804bf1a83bc6d5
Tao119 <tao.dama.art@gmail.com>
2024-05-18 10:57:59 +0900

    f

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -5,8 +5,9 @@ import { sql } from "@vercel/postgres";
 export async function POST(req: Request) {
   try {
     const res = await sql`SELECT ur_id, to_char(time, 'yyyy/mm/dd'), action FROM access_counter;`;
-    if (res) {
-      return new Response(JSON.stringify(convertToCamelCase({ success: true, data: res.rows })));
+    const res2 = await sql`SELECT id, ur_name, score, to_char(date, 'yyyy/mm/dd'), is_received FROM score where score >= 90000;`;
+    if (res && res2) {
+      return new Response(JSON.stringify(convertToCamelCase({ success: true, data: res.rows, data2: res2.rows })));
     } else {
       return new Response(
         JSON.stringify({ success: false, error: "登録できませんでした" })
